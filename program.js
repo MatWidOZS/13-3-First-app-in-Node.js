e60ea070cd7834e6f3c9e43599e3694dda40d694
Mateusz Widerski <mat.widerski@gmail.com>
2018-04-04 20:46:11 +0200

    Fix text

--- program.js
+++ program.js
@@ -2,7 +2,7 @@ process.stdin.setEncoding('utf-8');
 
 
 
-console.log('\n\nType one of these instructions: \n"/version" - see your Node version, \n"/language" - see your system language, \n"/exit" - quit app.');
+console.log('\n\nType one of these commands: \n"/version" - see your Node version, \n"/language" - see your system language, \n"/exit" - quit app.');
 
 process.stdin.on('readable', function() {
 	var input = process.stdin.read();

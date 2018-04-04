5b23c1ad0160c64be40dee1e2c0120fd991b6b7c
Mateusz Widerski <mat.widerski@gmail.com>
2018-04-04 14:41:27 +0200

    Add quit app function

--- program.js
+++ program.js
@@ -1,8 +1,18 @@
 process.stdin.setEncoding('utf-8'); //ustawienie enkodowania na UTF-8 (domyślnie szesnastkowe)
 
+console.log(process.env);
+
+console.log('\n\nType "/exit" to quit app');
+
 process.stdin.on('readable', function() {
 	var input = process.stdin.read(); //read - odczytanie, co użytkownik podał na wejściu
-	if(input !== null) {
-		process.stdout.write(input);		
+	if (input !== null) {
+		var instruction = input.toString().trim();
+		if (instruction === '/exit') {
+			process.stdout.write('Quitting app!\n');
+			process.exit();
+		} else {
+			process.stderr.write('Wrong instruction!\n');
+		}
 	}
 });

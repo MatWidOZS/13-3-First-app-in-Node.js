12309e7d69d34695e741c988320b0468fcc5a658
Mateusz Widerski <mat.widerski@gmail.com>
2018-04-04 14:44:17 +0200

    Delete unnecessary comments

--- program.js
+++ program.js
@@ -1,11 +1,11 @@
-process.stdin.setEncoding('utf-8'); //ustawienie enkodowania na UTF-8 (domyślnie szesnastkowe)
+process.stdin.setEncoding('utf-8');
 
 console.log(process.env);
 
 console.log('\n\nType "/exit" to quit app');
 
 process.stdin.on('readable', function() {
-	var input = process.stdin.read(); //read - odczytanie, co użytkownik podał na wejściu
+	var input = process.stdin.read();
 	if (input !== null) {
 		var instruction = input.toString().trim();
 		if (instruction === '/exit') {
